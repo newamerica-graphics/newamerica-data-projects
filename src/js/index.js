9c6b6686a3daf63279ec9d550dcb3021139f6c96
kjacks <kjack227@gmail.com>
2016-08-02 14:58:37 -0400

    added test for DOM targeting script

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
+import { HomegrownTerrorism } from "./projects/homegrown_terrorism.js";
+
 console.log("hello!");
+
+new HomegrownTerrorism();
+
---
+++ src/js/projects/homegrown_terrorism.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+export class HomegrownTerrorism {
+	constructor() {
+		$("#test1").append("This is the visualization!");
+	}
+}
