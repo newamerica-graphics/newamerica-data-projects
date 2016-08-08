7f20ae7d4d52a658683107754fadfd0eed259493
kjacks <kjack227@gmail.com>
2016-08-08 16:11:58 -0400

    added mouseover effects to map

--- src/js/components/us_states_map.js
+++ src/js/components/us_states_map.js
@@ -2,18 +2,45 @@ import $ from 'jquery';
 import Tabletop from 'tabletop';
 
 import {usStates} from '../../geography/us-states.js';
-console.log(usStates);
 
 let d3 = require("d3");
 
+let colorScale;
+
+
 export class usStatesMap {
-	constructor(id) {
-		console.log(id);
+	constructor(id, w) {
 		this.id = id;
+
+		this.svg = d3.select(id)
+					.append("svg");
+
+		this.setDimensions(w);
+	}
+
+	setDimensions(w) {
+		console.log(w);
+		this.w = w;
+		this.h = w/2;
+
+		this.svg
+			.attr("width", this.w)
+			.attr("height", this.h);
+
+		//Define map projection
+		let projection = d3.geoAlbersUsa()
+				.scale(this.w)
+				.translate([this.w/2, this.h/2]);
+
+		//Define path generator
+		this.pathGenerator = d3.geoPath()
+						 .projection(projection);
 	}
 
 	initialRender() {
+		console.log("in initial render");
 		let self = this;
+
 		Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1dGMIGe-14nOiMBf_BLtTTnzqTKelNl96fObhiqqb_HU/pubhtml',
                     callback: function(d) {
                    		self.buildGraph(d);
@@ -22,79 +49,61 @@ export class usStatesMap {
 	}
 
 	buildGraph(data) {
-		console.log(usStates);
-		//Width and height
-		var w = 500;
-		var h = 300;
-		//Define map projection
-		var projection = d3.geoAlbersUsa()
-							   .translate([w/2, h/2])
-							   .scale([500]);
-		//Define path generator
-		var path = d3.geoPath()
-						 .projection(projection);
-						 
+		console.log("rendering");
+
 		//Define quantize scale to sort data values into buckets of color
-		var color = d3.scaleQuantize()
-							.range(["rgb(237,248,233)","rgb(186,228,179)","rgb(116,196,118)","rgb(49,163,84)","rgb(0,109,44)"]);
-							//Colors taken from colorbrewer.js, included in the D3 download
-		
-		console.log(this.id);
-		//Create SVG element
-		var svg = d3.select(this.id)
-					.append("svg")
-					.attr("width", w)
-					.attr("height", h);
+		colorScale = d3.scaleQuantize()
+			.domain([
+				d3.min(data, function(d) { return d.value; }), 
+				d3.max(data, function(d) { return d.value; })
+			])
+			.range(["rgb(237,248,233)","rgb(186,228,179)","rgb(116,196,118)","rgb(49,163,84)","rgb(0,109,44)"]);
 
-		//Set input domain for color scale
-		color.domain([
-			d3.min(data, function(d) { return d.value; }), 
-			d3.max(data, function(d) { return d.value; })
-		]);
-
-		// Merge the ag. data and GeoJSON
-		// Loop through once for each ag. data value
 		for (var i = 0; i < data.length; i++) {
-	
 			//Grab state name
 			var dataState = data[i].state;
-			
 			//Grab data value, and convert from string to float
 			var dataValue = parseFloat(data[i].value);
-	
 			//Find the corresponding state inside the GeoJSON
 			for (var j = 0; j < usStates.features.length; j++) {
-
 				var jsonState = usStates.features[j].properties.name;
-
 				if (dataState == jsonState) {
 					//Copy the data value into the JSON
 					usStates.features[j].properties.value = dataValue;
-					
 					//Stop looking through the JSON
 					break;
-					
 				}
 			}		
 		}
 		// Bind data and create one path per GeoJSON feature
-		svg.selectAll("path")
+		this.paths = this.svg.selectAll("path")
 		   .data(usStates.features)
 		   .enter()
-		   .append("path")
-		   .attr("d", path)
-		   .style("fill", function(d) {
-		   		//Get data value
+		   .append("path");
+
+		this.paths.attr("d", this.pathGenerator)
+		    .style("fill", function(d) {
 		   		var value = d.properties.value;
-		   		
-		   		if (value) {
-		   			//If value exists…
-			   		return color(value);
-		   		} else {
-		   			//If value is undefined…
-			   		return "#ccc";
-		   		}
-		   });
+		   		return value ? colorScale(value) : "#ccc";
+		    }.bind(this))
+		    .on("mouseover", this.mouseover)
+		    .on("mouseout", this.mouseout);
+	}
+
+	updateDimensions(w) {
+		this.setDimensions(w);
+		this.paths.attr("d", this.pathGenerator);
+	}
+
+	mouseover() {
+		d3.select(this).style("fill", "orange");
+	}
+
+	mouseout() {
+		d3.select(this).style("fill", function(d) {
+	   		var value = d.properties.value;
+	   		return value ? colorScale(value) : "#ccc";
+	    });
 	}
 			
 }
