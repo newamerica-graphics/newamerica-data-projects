f701ec2e8d6efbfb7f0ccb23bb01708ea72a7880
kjacks <kjack227@gmail.com>
2016-09-08 18:09:39 -0400

    changed order of variables in gender viz

--- src/js/projects/homegrown_terrorism/index.js
+++ src/js/projects/homegrown_terrorism/index.js
@@ -13,7 +13,7 @@ let variables = {
 	field_kids: {"variable":"field_kids", "displayName":"Kids", "format":"string", "scaleType":"categorical", "color":"blue"},
 	age: {"variable":"age", "displayName":"Age", "format":"number", "scaleType":"categorical", "color":"turquoise"},
 	marital_status: {"variable":"marital_status", "displayName":"Marital Status", "scaleType":"categorical", "format":"string"},
-	gender: {"variable":"gender", "displayName":"Gender", "format":"string", "scaleType":"categorical", "color":"red"},
+	gender: {"variable":"gender", "displayName":"Gender", "format":"string", "scaleType":"categorical", "color":"red", "customDomain":["Female", "Male"], "customRange":[colors.red.light, colors.turquoise.light]},
 	field_year_indicted: {"variable":"field_year_indicted", "displayName":"Field Indicted", "format":"year", "scaleType":"categorical", "color":"blue"},
 	char_online_radicalization: {"variable":"char_online_radicalization", "format": "string"},
 	headshot: {"variable":"headshot", "format":"image"}
