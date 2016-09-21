83b28022d58f67a533fecf2e175a2caf1e99ba35
kjacks <kjack227@gmail.com>
2016-09-21 14:49:05 -0400

    set download link to just download png

--- src/js/layouts/chart.js
+++ src/js/layouts/chart.js
@@ -1,83 +1,58 @@
 import $ from 'jquery';
 
+import domtoimage from 'dom-to-image';
+
 let d3 = require("d3");
 
 
 // superclass for all chart types, adds download and share buttons to the chart
 export class Chart {
 	constructor(id, isSubComponent) {
-		this.id = id;
 		this.isSubComponent = isSubComponent;
 		if (isSubComponent) {
 			return;
 		}
 
-		this.downloadLink = d3.select(id + "__download-link");
-
-		this.downloadSVGButton = d3.select(id + "__download-svg")
-			
-			.attr("download", "chart.svg");
-
-		this.downloadPNGButton = d3.select(id + "__download-png")
-			
-			.attr("download", "chart.png");
-
-		this.canvas = d3.select(id)
-			.append("canvas")
-			.style("display", "none");
+		this.downloadLink = $(id + "__download-link")
+			.on("click", () => { this.setPngLink() });
 	}
 
 	render() {
-		if (this.isSubComponent) {
-			return;
-		}
-		// sets href link for svg button, triggered on mouseenter to encapsulate current state of the chart
-		this.downloadLink
-			.on("click", () => { this.setLinks() });
-
-
-		// sets href link for png button, triggered on mouseenter to encapsulate current state of the chart
-		// this.downloadPNGButton
-		// 	.on("mouseenter", () => { this.setLink("png"); });
 	}
 
-	setLinks() {
-		console.log("setting link!");
-		let svg = d3.select(this.id + " svg");
+	// setSvgLink() {
+	// 	console.log("setting link!");
 
-		let html = svg
-	        .attr("version", 1.1)
-	        .attr("xmlns", "http://www.w3.org/2000/svg")
-	        .node().parentNode.innerHTML;
+	// 	let svg = d3.select(this.id + " svg");
 
-	       console.log(html);
+	// 	let html = svg
+	//         .attr("version", 1.1)
+	//         .attr("xmlns", "http://www.w3.org/2000/svg")
+	//         .node().parentNode.innerHTML;
 
-		let imgsrc = 'data:image/svg+xml;base64,'+ btoa(html); 
+	//        console.log(html);
+
+	// 	let imgsrc = 'data:image/svg+xml;base64,'+ btoa(html); 
 		
-		this.downloadSVGButton
-			.attr("href", imgsrc);
-		// for png images, first sets canvas to dimensions of svg, renders the svg as an image on the canvas, then converts 
-		// 	the canvas to a dataURI
-		this.canvas
-			.attr("height", svg.attr("height"))
-			.attr("width", svg.attr("width"));
+	// 	this.downloadSvgButton
+	// 		.attr("href", imgsrc);
+	// }
 
-		let canvas = this.canvas._groups[0][0];
-
-	 	let context = canvas.getContext("2d");
-
-		let image = new Image;
-		image.src = imgsrc;
-		image.onload = () => {
-			context.drawImage(image, 0, 0);
-
-			let canvasdata = canvas.toDataURL("image/png");
-
-			this.downloadPNGButton
-				.attr("href", canvasdata);
-		};
+	setPngLink() {
+		this.id = this.id.replace("#", "");
 		
+		let node = document.getElementById(this.id);
 
-		
+		domtoimage.toPng(node)
+		    .then((dataUrl) => {
+		    	var link = document.createElement("a");
+		        link.download = 'chart.png';
+		        link.href = dataUrl;
+		        link.click();
+		        link.remove();
+		    })
+		    .catch(function (error) {
+		        console.error('oops, something went wrong!', error);
+		    });
 	}
 }
