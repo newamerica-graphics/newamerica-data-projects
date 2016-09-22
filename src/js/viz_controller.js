b86aaf7475cc9fcc441298d0bf887ab2248fab7b
kjacks <kjack227@gmail.com>
2016-09-22 14:12:10 -0400

    successfully implemented json and single csv download

--- src/js/viz_controller.js
+++ src/js/viz_controller.js
@@ -1,7 +1,9 @@
 require('./../scss/index.scss');
+var json2csv = require('json2csv');
 
 import $ from 'jquery';
 let d3 = require("d3");
+
 
 import { DotMatrix } from "./chart_types/dot_matrix.js";
 import { DotHistogram } from "./chart_types/dot_histogram.js";
@@ -86,15 +88,33 @@ export function setupProject(projectSettings) {
 	}
 
 	function setDownloadLinks(data) {
-		let publicData = {};
+		let publicDataJson = {};
 		for (let sheetName of dataSheetNames) {
-			publicData[sheetName] = data[sheetName];
+			publicDataJson[sheetName] = data[sheetName];
 		}
 
-		console.log(publicData);
+		console.log(publicDataJson);
 
-		$("#in-depth__download__xls").attr("href", projectSettings.downloadDataLink + "export?format=xlsx");
-		$("#in-depth__download__json").attr("href", projectSettings.dataUrl);
+		var fields = Object.keys(publicDataJson.state_data[0]);
+
+		console.log(publicDataJson.state_data);
+		console.log(fields);
+
+		try {
+			var result = json2csv({ data: publicDataJson.state_data, fields: fields });
+			console.log(result);
+			var csvDataUrlString = "data:text/csv;charset=utf-8," +escape(result);
+
+			$("#in-depth__download__csv").attr("href", csvDataUrlString);
+		} catch (err) {
+			// Errors are thrown for bad options, or if the data is empty and no fields are provided.
+			// Be sure to provide fields if it is possible that your data array will be empty.
+			console.error(err);
+		}
+
+		var jsonDataUrlString = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(publicDataJson));
+
+		$("#in-depth__download__json").attr("href", jsonDataUrlString);
 	}
 
 	function render() {
