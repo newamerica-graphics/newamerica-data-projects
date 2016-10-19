a381ec15224847e9e3003a4a1162c489ec974f04
kjacks <kjack227@gmail.com>
2016-10-19 14:52:40 -0400

    added carto instance running locally

---
+++ src/js/projects/financial_opportunity/index.js
@@ -0,0 +1,49 @@
+var L = require('leaflet');
+
+var mapboxAccessToken = "pk.eyJ1IjoibmV3YW1lcmljYW1hcGJveCIsImEiOiJjaXVmdTUzbXcwMGdsMzNwMmRweXN5eG52In0.AXO-coBbL621lzrE14xtEA";
+
+
+var map = L.map('map').setView([37.8, -96], 4);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+	maxZoom: 18,
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	id: 'mapbox.light'
+}).addTo(map);
+
+// get color depending on population density value
+function getColor(d) {
+	return d > 1000 ? '#800026' :
+			d > 500  ? '#BD0026' :
+			d > 200  ? '#E31A1C' :
+			d > 100  ? '#FC4E2A' :
+			d > 50   ? '#FD8D3C' :
+			d > 20   ? '#FEB24C' :
+			d > 10   ? '#FED976' :
+						'#FFEDA0';
+}
+
+function style(feature) {
+	return {
+		weight: 2,
+		opacity: 1,
+		color: 'white',
+		dashArray: '3',
+		fillOpacity: 0.7,
+		fillColor: getColor(feature.properties.density)
+	};
+}
+
+// var geojson = L.geoJson(statesData, {
+// 	style: style,
+// }).addTo(map);
+
+// L.geoJson(statesData).addTo(map);
+
+// var url = "https://jackson.carto.com/api/v2/viz/edf83036-8fea-11e6-8a5c-0e05a8b3e3d7/viz.json";
+
+// cartodb.createVis('map', url)
+// 	.done(function(vis, layers) {
+// 	});
