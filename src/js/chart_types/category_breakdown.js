4e390dfbd63543bf6b0fd12ae190237a809f8335
kjacks <kjack227@gmail.com>
2017-04-05 17:41:02 -0400

    added mouseover effect to category breakdown chart

--- src/js/chart_types/category_breakdown.js
+++ src/js/chart_types/category_breakdown.js
@@ -10,16 +10,16 @@ import { getColorScale } from "../helper_functions/get_color_scale.js";
 
 import { Tooltip } from "../components/tooltip.js";
 
-const categoryYPadding = 50,
+const categoryYPadding = 10,
 	textWidth = 200;
 
 export class CategoryBreakdown {
 	constructor(vizSettings, imageFolderId) {
 		Object.assign(this, vizSettings);
 	
-		this.svg = d3.select(this.id)
-			.append("svg")
-			.attr("width", "100%");
+		this.chartContainer = d3.select(this.id)
+			.append("div")
+			.attr("class", "category-breakdown");
 
 		this.currFilter = this.filterVars[0];
 		this.currFilterVar = this.filterVars[0].variable;
@@ -31,7 +31,6 @@ export class CategoryBreakdown {
 	}
 
 	render(data) {
-		
 		this.data = this.getDataNest(data[this.primaryDataSheet]);
 		console.log(this.data);
 		this.setDimensions();
@@ -55,6 +54,125 @@ export class CategoryBreakdown {
 		return d3.nest()
 			.key((d) => { return d[this.currFilterVar]})
 			.entries(data);
+	}
+
+	setScale(data) {
+		this.colorScale = getColorScale(data, this.currFilter);
+		console.log(this.colorScale.domain())
+		console.log(this.colorScale.range())
+	}
+
+	setDimensions() {
+		this.w = $(this.id).width() - textWidth;
+		this.numPerRow = Math.floor(this.w/(this.dotSettings.width + this.dotSettings.offset));
+
+		// this.h = this.setCategoryYTransforms();
+		
+		console.log(this.numPerRow);
+		// this.dotsPerCol = Math.ceil(this.dataLength/numCols);
+
+		// this.h = this.dotsPerCol * (this.dotSettings.width + this.dotSettings.offset);		
+		// this.svg
+		// 	.attr("height", this.h);
+		
+	}
+
+	setCategoryYTransforms() {
+		this.categoryYTransforms = [];
+		let currY = this.dotSettings.width/2;
+		let numRows;
+		for (let category of this.data) {
+			console.log(currY);
+			this.categoryYTransforms.push(currY);
+			numRows = Math.floor(category.values.length/this.numPerRow);
+			currY += categoryYPadding + numRows*(this.dotSettings.width + this.dotSettings.offset);
+		}
+
+		return currY;
+	}
+
+	buildGraph() {
+
+		this.categoryContainers = this.chartContainer.selectAll("div")
+			.data(this.data)
+			.enter().append("div")
+			.attr("class", "category-breakdown__category-container");
+			// .attr("transform", (d, i) => { return "translate(0," + this.categoryYTransforms[i] + ")"; })
+
+		this.textContainers = this.categoryContainers.append("div")
+			.attr("class", "category-breakdown__text")
+			
+		this.textContainers.append("h3")
+			.attr("class", "category-breakdown__text__heading")
+			.text((d) => { return d.key; });
+		
+		this.textContainers.append("h5")
+			.attr("class", "category-breakdown__text__subheading")
+			// .attr("transform", "translate(0,30)")
+			.text((d) => { return d.values.length + " states"; });
+
+
+		this.dataContainers = this.categoryContainers
+			.append("div")
+			.attr("class", "category-breakdown__data-container")
+			.append("svg")
+			.attr("class", "category-breakdown__data")
+			.attr("width", "100%")
+			.attr("height", (d) => {
+				let numRows = Math.ceil(d.values.length/this.numPerRow);
+				return numRows*(this.dotSettings.width + this.dotSettings.offset) + categoryYPadding;
+			})
+			// .attr("transform", "translate(" + (textWidth + this.dotSettings.width/2) + ")")
+
+		this.dataG = this.dataContainers.selectAll("g")
+			.data((d) => { console.log(d); return d.values; })
+			.enter().append("g")
+			.attr("transform", (d, i) => { return "translate(" + this.calcX(i) + "," + this.calcY(i) + ")"; })
+			.on("mouseover", (d, index, paths) => { return this.mouseover(d, paths[index], d3.event); })
+		    .on("mouseout", (d, index, paths) => { return this.mouseout(d, paths[index]); })
+		    .on("click", (d) => { return this.eventSettings.click && this.eventSettings.click.handlerFunc ? this.eventSettings.click.handlerFunc(d.id) : null; });
+
+		this.dataCircles = this.dataG.append("circle")
+			.attr("class", "category-breakdown__data__circle")
+			.attr("id", (d, i) => { return i; })
+			.attr("cx", this.dotSettings.width/2)
+			.attr("cy", this.dotSettings.width/2)
+			.attr("r", this.dotSettings.width/2)
+			.attr("fill", "white")
+			.attr("stroke", (d) => { return this.colorScale(d[this.currFilterVar]); })
+			
+
+		this.dataCircleText = this.dataG.append("text")
+			.attr("class", "category-breakdown__data__label")
+			.attr("id", (d, i) => { return i; })
+			.attr("x", this.dotSettings.width/2)
+			.attr("y", this.dotSettings.width/2)
+			.attr("fill", (d) => { return this.colorScale(d[this.currFilterVar]); })
+			.text((d) => { return d[this.labelVar.variable]; });
+
+		// this.cells = this.svg.selectAll("rect")
+		// 	.data(data)
+		// 	.enter().append("rect")
+		// 	.attr("width", this.dotSettings.width)
+		//     .attr("height", this.dotSettings.width)
+		//     .attr("x", (d, i) => { return this.calcX(d, i); })
+		//     .attr("y", (d, i) => { return this.calcY(i); })
+		//     .attr("fill", (d) => {
+		//     	return this.colorScale(d[this.currFilterVar]);
+		//     })
+		//     .style("cursor", this.eventSettings.click ? "pointer" : "auto")
+		//     .attr("class", (d) => { return d[this.currFilterVar]; })
+		//     .on("mouseover", (d, index, paths) => { return this.mouseover(d, paths[index], d3.event); })
+		//     .on("mouseout", (d, index, paths) => { return this.mouseout(paths[index]); })
+		//     .on("click", (d) => { return this.eventSettings.click && this.eventSettings.click.handlerFunc ? this.eventSettings.click.handlerFunc(d.id) : null; });
+	}
+
+	calcX(i) {
+		return (i%this.numPerRow)*(this.dotSettings.width + this.dotSettings.offset) + 2;
+	}
+
+	calcY(i) {
+		return Math.floor(i/this.numPerRow)*(this.dotSettings.width + this.dotSettings.offset) + 2; 
 	}
 
 	sortData() {
@@ -106,206 +224,49 @@ export class CategoryBreakdown {
 		}
 	}
 
-	setScale(data) {
-		this.colorScale = getColorScale(data, this.currFilter);
-		console.log(this.colorScale.domain())
-		console.log(this.colorScale.range())
-	}
-
-	setDimensions() {
-		this.w = $(this.id).width() - textWidth;
-		this.numPerRow = Math.floor(this.w/(this.dotSettings.width + this.dotSettings.offset));
-
-		this.h = this.setCategoryYTransforms();
-		
-		console.log(this.numPerRow);
-		// this.dotsPerCol = Math.ceil(this.dataLength/numCols);
-
-		// this.h = this.dotsPerCol * (this.dotSettings.width + this.dotSettings.offset);		
-		this.svg
-			.attr("height", this.h);
-		
-	}
-
-	setCategoryYTransforms() {
-		this.categoryYTransforms = [];
-		let currY = this.dotSettings.width/2;
-		let numRows;
-		for (let category of this.data) {
-			console.log(currY);
-			this.categoryYTransforms.push(currY);
-			numRows = Math.floor(category.values.length/this.numPerRow);
-			currY += categoryYPadding + numRows*(this.dotSettings.width + this.dotSettings.offset);
-		}
-
-		return currY;
-	}
-
-	buildGraph() {
-
-		this.categoryContainers = this.svg.selectAll("g")
-			.data(this.data)
-			.enter().append("g")
-			.attr("class", "category-breakdown__category-container")
-			.attr("transform", (d, i) => { return "translate(0," + this.categoryYTransforms[i] + ")"; })
-
-		this.textContainers = this.categoryContainers.append("g")
-			.attr("class", "category-breakdown__text")
-			
-			
-		this.textContainers.append("text")
-			.attr("class", "category-breakdown__text__heading")
-			.text((d) => { return d.key; });
-		
-		this.textContainers.append("text")
-			.attr("class", "category-breakdown__text__subheading")
-			.attr("transform", "translate(0,30)")
-			.text((d) => { return d.values.length + " states"; });
-
-
-		this.dataContainers = this.categoryContainers.append("g")
-			.attr("class", "category-breakdown__data")
-			.attr("transform", "translate(" + (textWidth + this.dotSettings.width/2) + ")")
-
-		this.dataCircles = this.dataContainers.selectAll("circle")
-			.data((d) => { console.log(d); return d.values; })
-			.enter().append("circle")
-			.attr("class", "category-breakdown__data__circle")
-			.attr("cx", (d, i) => { return this.calcX(i); })
-			.attr("cy", (d, i) => { return this.calcY(i); })
-			.attr("r", this.dotSettings.width/2)
-			.attr("fill", "white")
-			.attr("stroke", (d) => { return this.colorScale(d[this.currFilterVar]); });
-
-		this.dataCircleText = this.dataContainers.selectAll("text")
-			.data((d) => { console.log(d); return d.values; })
-			.enter().append("text")
-			.attr("class", "category-breakdown__data__label")
-			.attr("x", (d, i) => { return this.calcX(i); })
-			.attr("y", (d, i) => { return this.calcY(i); })
-			.attr("stroke", (d) => { return this.colorScale(d[this.currFilterVar]); })
-			.style("text-anchor", "middle")
-			.style("alignment-baseline", "middle")
-			.text((d) => { return d[this.labelVar.variable]; });
-
-		// this.cells = this.svg.selectAll("rect")
-		// 	.data(data)
-		// 	.enter().append("rect")
-		// 	.attr("width", this.dotSettings.width)
-		//     .attr("height", this.dotSettings.width)
-		//     .attr("x", (d, i) => { return this.calcX(d, i); })
-		//     .attr("y", (d, i) => { return this.calcY(i); })
-		//     .attr("fill", (d) => {
-		//     	return this.colorScale(d[this.currFilterVar]);
-		//     })
-		//     .style("cursor", this.eventSettings.click ? "pointer" : "auto")
-		//     .attr("class", (d) => { return d[this.currFilterVar]; })
-		//     .on("mouseover", (d, index, paths) => { return this.mouseover(d, paths[index], d3.event); })
-		//     .on("mouseout", (d, index, paths) => { return this.mouseout(paths[index]); })
-		//     .on("click", (d) => { return this.eventSettings.click && this.eventSettings.click.handlerFunc ? this.eventSettings.click.handlerFunc(d.id) : null; });
-	}
-	
-
-	setLegend() {
-		let { valCountType, showValCounts } = this.legendSettings;
-		let dataLength = this.data.length;
-		
-		if ( showValCounts ) {
-			this.legendSettings.valCounts = d3.nest()
-				.key((d) => { return d[this.currFilterVar]; })
-				.rollup(function(v) {
-					if (valCountType == "percent") {
-						return formatValue(v.length/dataLength, "percent"); 
-					} else if (valCountType == "both") {
-						return v.length + " (" + formatValue(v.length/dataLength, "percent") + ")";
-					} else {
-						return v.length;
-					}
-					
-				})
-				.map(this.data);
-		}
-
-		this.legendSettings.format = this.currFilter.format;
-		this.legendSettings.scaleType = this.currFilter.scaleType;
-		this.legendSettings.colorScale = this.colorScale;
-		this.legendSettings.valChangedFunction = this.changeVariableValsShown.bind(this);
-
-		this.legend.render(this.legendSettings);
-
-	}
-
-	calcX(i) {
-		return (i%this.numPerRow)*(this.dotSettings.width + this.dotSettings.offset);
-	}
-
-	calcY(i) {
-		return Math.floor(i/this.numPerRow)*(this.dotSettings.width + this.dotSettings.offset); 
-	}
-
 	resize() {
 		this.setDimensions();
 
-		this.categoryContainers
-			.attr("transform", (d, i) => { return "translate(0," + this.categoryYTransforms[i] + ")"; })
+		console.log(this.numPerRow);
 
-		this.dataCircles
-			.attr("cx", (d, i) => { return this.calcX(i); })
-			.attr("cy", (d, i) => { return this.calcY(i); });
-		
-		this.dataCircleText
-			.attr("x", (d, i) => { return this.calcX(i); })
-			.attr("y", (d, i) => { return this.calcY(i); });
+		this.dataContainers
+			.attr("height", (d) => {
+				let numRows = Math.ceil(d.values.length/this.numPerRow);
+				return numRows*(this.dotSettings.width + this.dotSettings.offset) + categoryYPadding;
+			})
+
+		this.dataG
+			.attr("transform", (d, i) => { return "translate(" + this.calcX(i) + "," + this.calcY(i) + ")"; })
 	}
-
-	// changeFilter(colorVar, scaleType) {
-	// 	this.currFilterVar = this.currFilterVariable;
-	// 	this.scaleType = scaleType;
-
-	// 	this.sortData();
-	// 	this.setScale();
-	// 	this.cells.remove();
-	// 	this.buildGraph();
-	// }
 
 	mouseover(datum, path, eventObject) {
 		let mousePos = [];
 		mousePos[0] = eventObject.pageX;
 		mousePos[1] = eventObject.pageY;
 
-		let mouseoverSettings = this.eventSettings.mouseover;
-
 		let elem = d3.select(path);
-		// let prevX = elem.attr("x");
-		// let prevY = elem.attr("y");
+		elem.select("circle")
+			.attr("fill", (d) => {
+		    	return this.colorScale(datum[this.currFilterVar]);
+		    });
 
-		let currFill = elem.attr("fill");
-		elem
-			// .attr("width", this.dotSettings.width * 2)
-		 //    .attr("height", this.dotSettings.width * 2)
-		 //    .attr("x", prevX - this.dotSettings.width/2)
-		 //    .attr("y", prevY - this.dotSettings.width/2)
-		 	.attr("fill", (d) => {
-		    	return mouseoverSettings.fill ? mouseoverSettings.fill : currFill;
-		    })
-			.attr("stroke", mouseoverSettings.stroke ? mouseoverSettings.stroke : "none")
-			.attr("stroke-width", mouseoverSettings.strokeWidth ? mouseoverSettings.strokeWidth : "0px");
+		elem.select("text")
+			.attr("fill", "white");
+
 			
 		this.tooltip ? this.tooltip.show(datum, mousePos) : null;
 	}
 
-	mouseout(path) {
+	mouseout(datum, path) {
 		let elem = d3.select(path);
-		// let prevX = Number(elem.attr("x"));
-		// let prevY = Number(elem.attr("y"));
+		elem.select("circle")
+			.attr("fill", "white");
 
-		let currFill = elem.attr("fill");
-
-		elem
+		elem.select("text")
 			.attr("fill", (d) => {
-		    	return currFill;
-		    })
-			.attr("stroke", "none");
+		    	return this.colorScale(datum[this.currFilterVar]);
+		    });
+
 			// .attr("width", this.dotSettings.width)
 		 //    .attr("height", this.dotSettings.width)
 		 //    .attr("x", prevX + this.dotSettings.width/2)
