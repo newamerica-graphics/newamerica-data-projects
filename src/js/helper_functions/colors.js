d41aff3e5fa1c2509f019504741f32bf90634971
kjacks <jackson@newamerica.org>
2017-05-01 13:13:05 -0400

    added center director data

--- src/js/helper_functions/colors.js
+++ src/js/helper_functions/colors.js
@@ -1,5 +1,5 @@
 export var colors = {
-	"turquoise":{ "light": "#2EBCB3", "medium": "#1A8A84", "dark": "#005753"},
+	"turquoise":{ "very_light": "#97DED9", "light": "#2EBCB3", "medium": "#1A8A84", "dark": "#005753"},
 	"blue": { "light": "#5BA4DA", "medium": "#4378A0","dark": "#234A67"},
 	"red": { "light": "#E75C64", "medium": "#A64046","dark": "#692025"},
 	"purple": { "very_light": "#bd9fc6","light": "#A076AC", "medium": "#74557E","dark": "#48304F"},
