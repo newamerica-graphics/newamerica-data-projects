cda300313e20d76985fea0ba16519f24e5cf66dc
kjacks <kjack227@gmail.com>
2017-06-01 18:02:55 -0400

    implemented vertical timeline

--- src/js/chart_types/vertical_timeline.js
+++ src/js/chart_types/vertical_timeline.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 let d3 = require("d3");
 
 import { colors } from "../helper_functions/colors.js";
+import { getColorScale } from "../helper_functions/get_color_scale.js";
 
 export class VerticalTimeline {
 	constructor(vizSettings) {
@@ -13,17 +14,34 @@ export class VerticalTimeline {
 			.attr("class", "vertical-timeline");
 
 		this.xScale = d3.scaleBand();
+
+		this.windowHeight = $(window).height();
+		console.log("windowHeight", this.windowHeight);
+
+		$(window).scroll(() => { return this.scrollListener(); });
 	}
 
 	render(data) {
 		console.log(data);
 		this.data = data[this.primaryDataSheet];
+
+		this.dataNest = d3.nest()
+			.key((d) => { return d[this.timeVar.variable]; })
+			.sortKeys((a, b) => { console.log(a); return d3.ascending(+a, +b); })
+			.key((d) => { console.log(d); return d[this.categoryVar.variable]; })
+			.entries(this.data);
+
+		console.log(this.dataNest);
+
 		console.log(this.data);
-		this.categories = d3.map(this.data, (d) => { console.log(d); return d[this.categoryVar.variable];}).keys();
+		this.categories = d3.map(this.data, (d) => { return d[this.categoryVar.variable];}).keys();
 		this.setDimensions();
+
+		this.colorScale = getColorScale(this.data, this.categoryVar);
 		this.xScale.domain(this.categories);
 		console.log(this.categories);
 
+		this.appendCategoryTitles();
 		this.appendLines();
 		this.appendItems();
 	}
@@ -38,6 +56,26 @@ export class VerticalTimeline {
 
 	}
 
+	appendCategoryTitles() {
+		this.categoryTitleContainer = this.container.append("div")
+			.attr("class", "vertical-timeline__category-title-container");
+
+		this.categoryTitle = this.categoryTitleContainer.selectAll(".vertical-timeline__category-title")
+			.data(this.categories)
+			.enter().append("div")
+			.attr("class", "vertical-timeline__category-title")
+
+		this.categoryTitle.append("img")
+			.attr("class", "vertical-timeline__category-title__image")
+			.style("width", this.w/this.categories.length + "px")
+			.attr("src", (d, i) => { return this.categoryImageUrl + this.categoryVar.categoryImagePaths[i];});
+
+		this.categoryTitle.append("div")
+			.attr("class", "vertical-timeline__category-title__text")
+			.style("color", (d) => { return this.colorScale(d); })
+			.text((d) => { return d;});
+	}
+
 	appendLines() {
 		this.timelineLines = this.container.selectAll(".vertical-timeline__line")
 			.data(this.categories)
@@ -47,21 +85,66 @@ export class VerticalTimeline {
 	}
 
 	appendItems() {
-		this.timelineItems = this.container.selectAll("div")
-			.data(this.data)
+		this.timelineRows = this.container.selectAll(".vertical-timeline__row")
+			.data(this.dataNest)
+			.enter().append("div")
+			.attr("class", "vertical-timeline__row")
+			// .append("div")
+			// .attr("class", "vertical-timeline__row__inner-wrapper")
+
+		this.timelineItemGroups = this.timelineRows.selectAll(".vertical-timeline__item-group")
+			.data(d => d.values)
+			.enter().append("div")
+			.attr("class", "vertical-timeline__item-group")
+			.style("left", (d, i) => {
+				let shiftFactor = this.categories.indexOf(d.key) - i;
+				return shiftFactor*this.xScale.bandwidth() + "px"; 
+			})
+			.style("max-width", this.w/this.categories.length + "px")
+
+		this.timelineItemGroups.append("h5")
+			.attr("class", "vertical-timeline__time")
+			.style("color", (d) => { return this.colorScale(d.key); })
+			.text((d) => { return d.values[0][this.timeVar.variable] + this.timeSuffix; });
+
+		this.timelineItems = this.timelineItemGroups.selectAll(".vertical-timeline__item")
+			.data(d => d.values)
 			.enter().append("div")
 			.attr("class", "vertical-timeline__item")
-			.style("left", (d) => { console.log(d); return this.xScale(d[this.categoryVar.variable]) + "px"; })
-			.style("max-width", this.w/this.categories.length + "px");
 
-		this.timelineItems.append("h5")
-			.attr("class", "vertical-timeline__time")
-			.text((d) => { return d[this.timeVar.variable]; });
-
-		this.timelineItems.append("h5")
+		this.descriptionText = this.timelineItems.append("h5")
 			.attr("class", "vertical-timeline__description")
-			.text((d) => { return d[this.descriptionVar.variable]; });
+			.text((d) => { return d[this.descriptionVar.variable]; })
+			// .style("font-size", (d, index, paths) => {
+			// 	let elemOffset = paths[index].offsetHeight;
+			// 	console.log(this.windowHeight);
+			// 	console.log(elemOffset);
+			// 	return ((this.windowHeight - elemOffset)/10) + "px"; 
+			// });
+			// .text((d, index, paths) => {
+			// 	let elemOffset = paths[index].offsetHeight;
+			// 	console.log(paths[index]);
+			// 	console.log(paths[index].getBoundingClientRect());
+			// 	console.log($(paths[index]));
+			// 	return paths[index].getBoundingClientRect().top; 
+			// });
 
 
+	}
+
+	scrollListener() {
+		console.log(window);
+
+		this.descriptionText
+			.style("font-size", (d, index, paths) => {
+				let distanceFromBottom = this.windowHeight - paths[index].getBoundingClientRect().top - 30;
+				if (distanceFromBottom > 200) {
+					return "20px";
+				} else if (distanceFromBottom < 0) {
+					return "0px";
+				} else {
+					return (distanceFromBottom/10) + "px";
+				}
+			});
 	}
 }
--- src/js/projects/paid_family_leave_timeline/index.js
+++ src/js/projects/paid_family_leave_timeline/index.js
@@ -5,7 +5,7 @@ import { colors } from "../../helper_functions/colors.js";
 
 let variables = {
 	week: {"variable":"week", "displayName":"Week", "format": "string"},
-	category: {"variable":"category", "displayName":"Category", "format": "string"},
+	category: {"variable":"category", "displayName":"Category", "format": "string", "scaleType":"categorical", "customDomain":["PFL Milestones", "Maternal Health Milestones", "Infant Health Milestones"], "customRange":[colors.turquoise.light, colors.blue.light, colors.purple.light], "categoryImagePaths": ["placeholder_icon.svg", "placeholder_icon.svg", "placeholder_icon.svg"]},
 	description: {"variable":"description", "displayName":"Description", "format": "string"},
 }
 
@@ -15,7 +15,9 @@ let vizSettingsList = [
 		vizType: "vertical_timeline",
 		primaryDataSheet: "timeline",
 		timeVar: variables.week,
+		timeSuffix: " weeks",
 		categoryVar: variables.category,
+		categoryImageUrl: "https://s3-us-west-2.amazonaws.com/na-data-projects/images/paid_family_leave_timeline/",
 		descriptionVar: variables.description
 	},
 	// {
