cd6d528293ef399c63c5b1cfcc2fc17f07abc393
kjacks <jackson@newamerica.org>
2017-10-03 16:12:41 -0400

    updated definition explorer

--- src/js/projects/bretton_woods_asset_allocator/index.js
+++ src/js/projects/bretton_woods_asset_allocator/index.js
@@ -4,10 +4,10 @@ import { colors } from "../../helper_functions/colors.js";
 
 
 let variables = {
-	fund_name: {"variable":"fund_name", "displayName":"Fund Name", "format":"string", "category": "Basic Info"},
+	fund_name: {"variable":"fund_name", "displayName":"Fund Name", "format":"string"},
 	country: {"variable":"country", "displayName":"Country", "format":"string", "category":"Basic Info"},
-	aum_billions: {"variable":"aum_billions", "displayName":"AUM (Billions)", "format":"price", "category": "Basic Info", "scaleType":"linear", "customDomain": []},
-	type: {"variable":"type", "displayName":"Type", "format":"string", "category": "Basic Info"},
+	aum_bn: {"variable":"aum_bn", "displayName":"AUM (Billions)", "format":"price", "scaleType":"linear", "customDomain": []},
+	type: {"variable":"type", "displayName":"Type", "format":"string"},
 	top_twenty: {"variable":"top_twenty", "displayName":"Fund Type", "format":"string", "category":"Top Twenty Funds", "scaleType": "categorical", "customDomain":["Top Twenty Fund", "Other Fund"], "customRange":[colors.turquoise.light, colors.black]},
 	cumulative_score: {"variable":"cumulative_score", "displayName":"Cumulative Score", "format":"number", "scaleType": "quantize", "numBins":5, "customDomain": [0, 100], "customRange": [colors.white, colors.turquoise.light, colors.turquoise.dark]},	
 
@@ -25,82 +25,62 @@ let variables = {
 	development_2: {"variable":"development_2", "displayName":"Development 2", "format":"number", "scaleType": "categorical", "colorTable": true, "customDomain": ["Passed", "Failed"], "customRange": [colors.turquoise.light, colors.red.light]},
 
 	title: {"variable":"title", "displayName":"Title", "format":"string"},
-	description: {"variable":"description", "displayName":"Description", "format":"string"},
-	category: {"variable":"category", "displayName":"Cateogry", "format":"string"},
+	criteria: {"variable":"criteria", "displayName":"Criteria", "format":"string"},
+	criteria_description: {"variable":"criteria_description", "displayName":"Description", "format":"string"},
+	example: {"variable":"example", "displayName":"Example", "format":"string"},
+	category: {"variable":"category", "displayName":"Category", "format":"string"},
 }
 
 let vizSettingsList = [
-	{
-		id: "#asset-allocator__world-map",
-		vizType: "pindrop_map",
-		primaryDataSheet: "top_twenty",
-		// isMessagePasser: true,
-		// messageHandlerType: "change_value",
-		geometryType: "world",
-		stroke: {"color": colors.white, "width":"1", "opacity": "1", "hoverColor": colors.white, "hoverWidth": "1", hoverOpacity: ".6"},
-		pinRadius: 5,
-		idVar: variables.fund_name,
-		filterVars: [ variables.top_twenty],
-		radiusVar: variables.aum_billions,
-		tooltipVars: [ variables.fund_name, variables.country, variables.aum_billions, variables.type, variables.cumulative_score, variables.disclosure, variables.intention, variables.clarity, variables.integration, variables.implementation, variables.implementation_2, variables.commitment, variables.accountability, variables.partnership, variables.standards, variables.development, variables.development_2 ],
-		filterGroupSettings: {"hidden": false},
-		legendSettings: {"orientation": "horizontal-center", "showTitle": true},
-		zoomable: true,
-		// clickToProfile: { "variable": variables.program.variable, "url": "https://www.newamerica.org/in-depth/integrating-technology-early-literacy/program-profile/?" }
-	},
-	{
-		id: "#asset-allocator__world-map-all",
-		vizType: "pindrop_map",
-		primaryDataSheet: "funds",
-		// isMessagePasser: true,
-		// messageHandlerType: "change_value",
-		geometryType: "world",
-		stroke: {"color": colors.white, "width":"1", "opacity": "1", "hoverColor": colors.white, "hoverWidth": "1", hoverOpacity: ".6"},
-		pinRadius: 5,
-		idVar: variables.fund_name,
-		filterVars: [ variables.cumulative_score, variables.disclosure, variables.intention, variables.clarity, variables.integration, variables.implementation, variables.implementation_2, variables.commitment, variables.accountability, variables.partnership, variables.standards, variables.development, variables.development_2  ],
-		radiusVar: variables.aum_billions,
-		tooltipVars: [ variables.fund_name, variables.country, variables.aum_billions, variables.type, variables.cumulative_score, variables.disclosure, variables.intention, variables.clarity, variables.integration, variables.implementation, variables.implementation_2, variables.commitment, variables.accountability, variables.partnership, variables.standards, variables.development, variables.development_2 ],
-		filterGroupSettings: {"hidden": false},
-		legendSettings: {"orientation": "horizontal-center", "showTitle": true},
-		zoomable: true,
-		// clickToProfile: { "variable": variables.program.variable, "url": "https://www.newamerica.org/in-depth/integrating-technology-early-literacy/program-profile/?" }
-	},
-	{
-		id: "#asset-allocator__top-twenty-list",
-		primaryDataSheet: "top_twenty",
-		vizType: "table",
-		filterInitialDataFunction: (d) => {
-			console.log(d)
-			return d.top_twenty == "Top Twenty Fund";
-		},
-		tableVars: [ variables.fund_name, variables.country, variables.aum_billions, variables.type, variables.disclosure, variables.intention, variables.clarity, variables.integration, variables.implementation, variables.implementation_2, variables.commitment, variables.accountability, variables.partnership, variables.standards, variables.development, variables.development_2, variables.cumulative_score ],
-		defaultOrdering: [16, "desc"],
-		pagination: false,
-		numPerPage: 20,
-		freezeColumn: {leftColumns: 1},
-		colorVals: true
-	},
-	{
-		id: "#asset-allocator__disclosure", 
-		vizType: "category_breakdown",
-		primaryDataSheet: "funds",
-		dotSettings: { "width": 12, "offset": 3},
-		filterVars: [ variables.disclosure ],
-		tooltipVars: [ variables.fund_name, variables.country, variables.aum_billions, variables.type, variables.cumulative_score, variables.disclosure],
-		quantityLabel: "funds",
-		idVar: variables.fund_name,
-	},
-	{
-		id: "#asset-allocator__intention", 
-		vizType: "category_breakdown",
-		primaryDataSheet: "funds",
-		dotSettings: { "width": 12, "offset": 3},
-		filterVars: [ variables.intention ],
-		tooltipVars: [ variables.fund_name, variables.country, variables.aum_billions, variables.type, variables.cumulative_score, variables.intention],
-		quantityLabel: "funds",
-		idVar: variables.fund_name,
-	},
+	// {
+	// 	id: "#asset-allocator__world-map",
+	// 	vizType: "pindrop_map",
+	// 	primaryDataSheet: "funds",
+	// 	// isMessagePasser: true,
+	// 	// messageHandlerType: "change_value",
+	// 	geometryType: "world",
+	// 	stroke: {"color": colors.white, "width":"1", "opacity": "1", "hoverColor": colors.white, "hoverWidth": "1", hoverOpacity: ".6"},
+	// 	pinRadius: 5,
+	// 	idVar: variables.fund_name,
+	// 	filterVars: [ variables.top_twenty],
+	// 	radiusVar: variables.aum_bn,
+	// 	tooltipVars: [ variables.fund_name, variables.country, variables.aum_bn, variables.type, variables.cumulative_score ],
+	// 	filterGroupSettings: {"hidden": false},
+	// 	legendSettings: {"orientation": "horizontal-center", "showTitle": true},
+	// 	zoomable: true,
+	// 	// clickToProfile: { "variable": variables.program.variable, "url": "https://www.newamerica.org/in-depth/integrating-technology-early-literacy/program-profile/?" }
+	// },
+	// {
+	// 	id: "#asset-allocator__top-twenty-list",
+	// 	primaryDataSheet: "funds",
+	// 	vizType: "table",
+	// 	tableVars: [ variables.fund_name, variables.country, variables.aum_bn, variables.type, variables.disclosure, variables.intention, variables.clarity, variables.integration, variables.implementation, variables.implementation_2, variables.commitment, variables.accountability, variables.partnership, variables.standards, variables.development, variables.development_2, variables.cumulative_score ],
+	// 	defaultOrdering: [16, "desc"],
+	// 	pagination: false,
+	// 	numPerPage: 20,
+	// 	freezeColumn: {leftColumns: 1},
+	// 	colorVals: true
+	// },
+	// {
+	// 	id: "#asset-allocator__disclosure", 
+	// 	vizType: "category_breakdown",
+	// 	primaryDataSheet: "funds",
+	// 	dotSettings: { "width": 12, "offset": 3},
+	// 	filterVars: [ variables.disclosure ],
+	// 	tooltipVars: [ variables.fund_name, variables.country, variables.aum_bn, variables.type, variables.cumulative_score, variables.disclosure],
+	// 	quantityLabel: "funds",
+	// 	idVar: variables.fund_name,
+	// },
+	// {
+	// 	id: "#asset-allocator__intention", 
+	// 	vizType: "category_breakdown",
+	// 	primaryDataSheet: "funds",
+	// 	dotSettings: { "width": 12, "offset": 3},
+	// 	filterVars: [ variables.intention ],
+	// 	tooltipVars: [ variables.fund_name, variables.country, variables.aum_bn, variables.type, variables.cumulative_score, variables.intention],
+	// 	quantityLabel: "funds",
+	// 	idVar: variables.fund_name,
+	// },
 	
 ]
 
@@ -110,8 +90,9 @@ let reactVizSettingsList = [
 		vizType: "definition_explorer",
 		primaryDataSheet: "metric_definitions",
 		titleVar: variables.title,
-		descriptionVar: variables.description,
+		descriptionVars: [variables.criteria, variables.criteria_description],
 		categoryVar: variables.category,
+		sorting: "numerical"
 	},
 ]
 
--- src/js/react_chart_types/definition_explorer/DefinitionExplorer.js
+++ src/js/react_chart_types/definition_explorer/DefinitionExplorer.js
@@ -4,14 +4,13 @@ import { colors } from "../../helper_functions/colors.js";
 
 import { formatValue } from "../../helper_functions/format_value.js";
 
-import CategoryBlock from './CategoryBlock.js';
+import DefinitionEntry from './DefinitionEntry.js';
 
 const d3 = require("d3");
 
 class DefinitionExplorer extends React.Component {
 	constructor(props) {
 		super(props);
-		console.log(props);
 
 		this.data = props.data[props.vizSettings.primaryDataSheet];
 		this.getCategoryNest();
@@ -24,24 +23,28 @@ class DefinitionExplorer extends React.Component {
 			.key((d) => { return d[categoryVar.variable]})
 			.map(this.data)
 
-		console.log(this.categoryData)
-		console.log(this.categoryData.get("null"))
-		this.categoryTitles = this.categoryData.get("null").sort((a, b) => { return a[titleVar.variable] > b[titleVar.variable]; });
+		this.categoryTitles = this.categoryData.get("null")
+			.sort((a, b) => { 
+				if (this.props.vizSettings.sorting === "numerical") {
+					return +a[titleVar.variable].substring(0, 2) > +b[titleVar.variable].substring(0, 2); 
+				} else {
+					return a[titleVar.variable] > b[titleVar.variable]; 
+				}
+			});
 	}
 
 	render() {
-		const { titleVar, descriptionVar, chartTitle } = this.props.vizSettings;
+		const { titleVar, descriptionVars, chartTitle } = this.props.vizSettings;
 
 		return (
 			<div className="definition-explorer">
-				<div className="definition-explorer__category-container">
-					{this.categoryTitles.map((d) => {
-						let title = d[titleVar.variable],
-							description = d[descriptionVar.variable], 
-							definitions = this.categoryData.get(title) ? this.categoryData.get(title).sort((a, b) => { return a[titleVar.variable] > b[titleVar.variable]; }) : null;
+				<div className="definition-explorer__entry-container">
+					{this.categoryTitles.map((entryData) => {
+						let title = entryData.title,
+							subentrySettings = this.categoryData.get(title) ? this.categoryData.get(title).sort((a, b) => { return a[titleVar.variable] > b[titleVar.variable]; }) : null;
 
 						return (
-							<CategoryBlock key={title} title={title} description={description} categoryDefinitions={definitions} />
+							<DefinitionEntry key={title} title={title} descriptionVars={descriptionVars} entryData={entryData} subentrySettings={subentrySettings} />
 						)
 					})}
 				</div>
