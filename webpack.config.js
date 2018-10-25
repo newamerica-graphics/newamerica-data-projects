557c6db4459d1f2a86c823d907c7374f15e4ae84
lorenries <lorenries@users.noreply.github.com>
2018-10-25 11:22:22 -0400

    remove s3 plugin so webpack will build

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
-var webpack = require('webpack');
-var path = require('path');
-var S3Plugin = require('webpack-s3-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var webpack = require("webpack");
+var path = require("path");
+var S3Plugin = require("webpack-s3-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
-var BUILD_DIR = path.resolve(__dirname, 'build/');
-var PROJECT_DIR = path.resolve(__dirname, 'src');
+var BUILD_DIR = path.resolve(__dirname, "build/");
+var PROJECT_DIR = path.resolve(__dirname, "src");
 
 // s3 bucket settings
 var AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
@@ -14,36 +15,48 @@ var AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 var DATA_PROJECTS_S3_BUCKET_NAME = process.env.DATA_PROJECTS_S3_BUCKET_NAME;
 
 const config = env => {
-  console.log(env)
-  var {vizSettings} = require("./src/js/projects/" + env.project + "/settings.js")
+  console.log(env);
+  var { vizSettings } = require("./src/js/projects/" +
+    env.project +
+    "/settings.js");
 
   return {
-    entry: env.NODE_ENV === "development" ? PROJECT_DIR + "/js/index-dev.js" : PROJECT_DIR + "/js/index.js",
+    entry:
+      env.NODE_ENV === "development"
+        ? PROJECT_DIR + "/js/index-dev.js"
+        : PROJECT_DIR + "/js/index.js",
     output: {
       path: BUILD_DIR,
-      filename: env.project + '.js'
+      filename: env.project + ".js"
     },
     resolve: {
-      extensions: ['.js'],
+      extensions: [".js"],
       alias: {
-        'mapbox-gl': path.resolve('./node_modules/mapbox-gl/dist/mapbox-gl.js'),
-        'mapbox-gl-geocoder': path.resolve('./node_modules/mapbox-gl/plugins/src/mapbox-gl-geocoder/v2.0.1/'),
+        "mapbox-gl": path.resolve("./node_modules/mapbox-gl/dist/mapbox-gl.js"),
+        "mapbox-gl-geocoder": path.resolve(
+          "./node_modules/mapbox-gl/plugins/src/mapbox-gl-geocoder/v2.0.1/"
+        )
       }
     },
-    devtool: env.NODE_ENV === "development" ? 'cheap-module-eval-source-map' : 'eval',
+    devtool:
+      env.NODE_ENV === "development" ? "cheap-module-eval-source-map" : "eval",
     devServer: {
       contentBase: "./"
     },
     module: {
-      rules : [
+      rules: [
         {
-          test : /\.js?/,
-          include : PROJECT_DIR,
-          use : 'babel-loader'
+          test: /\.js?/,
+          include: PROJECT_DIR,
+          use: "babel-loader"
         },
         {
           test: /\.scss$/,
-          use: [ {loader: 'style-loader'}, {loader: 'css-loader'}, {loader:'sass-loader'} ]
+          use: [
+            { loader: "style-loader" },
+            { loader: "css-loader" },
+            { loader: "sass-loader" }
+          ]
         },
         {
           test: /\.png$/,
@@ -59,50 +72,39 @@ const config = env => {
         },
         {
           test: /mapbox-gl.+\.js$/,
-          use: 'transform-loader/cacheable?brfs'
-        },
+          use: "transform-loader/cacheable?brfs"
+        }
       ]
     },
-    plugins: (env.NODE_ENV === 'development') ?
-      [
-        // new BundleAnalyzerPlugin(),
-        new HtmlWebpackPlugin({
-          title: env.project,
-          vizIdList: Object.keys(vizSettings),
-          template: './index_dev.ejs',
-          filename: '../index-dev.html'
-        }),
-        new webpack.DefinePlugin({
-          PROJECT: JSON.stringify(env.project)
-        })
-      ] : [
-        new UglifyJSPlugin({
-          sourceMap: true
-        }),
-        new webpack.DefinePlugin({
-          PROJECT: JSON.stringify(env.project)
-        }),
-        new S3Plugin({
-          // Only upload css and js
-          include: /.*\.(scss|css|js)/,
-          // s3Options are required
-          s3Options: {
-            accessKeyId: AWS_ACCESS_KEY_ID,
-            secretAccessKey: AWS_SECRET_ACCESS_KEY,
-          },
-          s3UploadOptions: {
-            Bucket: DATA_PROJECTS_S3_BUCKET_NAME
-          }
-        }),
-      ],
+    plugins:
+      env.NODE_ENV === "development"
+        ? [
+            // new BundleAnalyzerPlugin(),
+            new HtmlWebpackPlugin({
+              title: env.project,
+              vizIdList: Object.keys(vizSettings),
+              template: "./index_dev.ejs",
+              filename: "../index-dev.html"
+            }),
+            new webpack.DefinePlugin({
+              PROJECT: JSON.stringify(env.project)
+            })
+          ]
+        : [
+            new UglifyJSPlugin({
+              sourceMap: true
+            }),
+            new webpack.DefinePlugin({
+              PROJECT: JSON.stringify(env.project)
+            })
+          ],
     node: {
       fs: "empty",
-      net: 'empty',
-      tls: 'empty',
+      net: "empty",
+      tls: "empty",
       console: true
     }
-  }
+  };
 };
-
 
 module.exports = config;
